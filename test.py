58d8a82d76e85f84c33074a20f5b8d39fe7dd2b2
Anish Sanghvi <asanghvi@cciltmac4145.local>
2024-10-22 11:56:51 +1100

    manipulating maf file

--- test.py
+++ test.py
@@ -9,7 +9,9 @@ from sklearn.metrics import PrecisionRecallDisplay, confusion_matrix, ConfusionM
 from sklearn.metrics import mean_absolute_error, median_absolute_error, explained_variance_score
 from sklearn.metrics import precision_score, recall_score, f1_score
 
-# Load the dataset
+#############################
+#### Signature Database #####
+#############################
 folder_path = '../../../Downloads/new_sigs/'
 sig_data = pd.DataFrame()
 
@@ -36,76 +38,95 @@ sig_data["p53 status"] = ""
 print(sig_data)
 print("\n--------------------------\n")
 
-############ downloaded maf
+#############################
+######### MAF FILE ##########
+#############################
+maf_file_path = '../../../Downloads/October_2016_whitelist_2583.snv_mnv_indel.maf.xena.nonUS'
+maf_data = pd.read_csv(maf_file_path, sep='\t', comment='#', low_memory=False)
 
-# maf_file_path = '../../../Downloads/October_2016_whitelist_2583.snv_mnv_indel.maf.xena.nonUS'
-# maf_data = pd.read_csv(maf_file_path, sep='\t', comment='#')
-# # print(maf_data)
-# try_filter = maf_data.query('gene == "TP53"')
-# print(try_filter)
+filtered_data = maf_data[(maf_data['start'] == maf_data['end']) & (maf_data['chr'] == '1') & (maf_data['alt'] != '-') ]
+filtered_data['HG19_Variant'] = 'chr17:g.' + \
+                                filtered_data['start'].astype(str) + \
+                                filtered_data['reference'] + '>' + \
+                                filtered_data['alt']
 
-############# EU  File
-new_file_tester =  '../../../Downloads/UMD_variants_EU.xlsx'
-tester_data =  pd.read_excel(new_file_tester)
-temp = tester_data[tester_data['Mutational_event'].str.contains('^[A, C, G, T]>[A, C, G, T]$') & 
-                    tester_data['WT_Codon'].str.contains('^[ACGT]{3}$') & 
-                    tester_data['Mutant_Codon'].str.contains('^[ACGT]{3}$')]
+result = filtered_data[['Sample', 'HG19_Variant', 'gene', 'effect']]
 
-temp['Signature'] = temp.apply(lambda row: row['Mutant_Codon'][0] + '[' + row['Mutational_event'] + ']' + row['Mutant_Codon'][2], axis=1)
-filtered = temp[['Variant_Classification', 'Pathogenicity', 'Final comment',  'Signature']]
-
-grouped_signature_data = filtered.groupby(['Signature', 'Pathogenicity']).size().reset_index(name='Count')
-
-pathogenicity_mapping = {
-    'Pathogenic': 1,
-    'Likely Pathogenic': 0.75,
-    'Possibly pathogenic': 0.5,
-    'VUS': 0.25,
-    'Benign': 0
-}
-
-grouped_signature_data['Pathogenicity_Score'] = grouped_signature_data['Pathogenicity'].map(pathogenicity_mapping)
-
-weighted_likelihood = grouped_signature_data.groupby('Signature').apply(
-    lambda x: (x['Count'] * x['Pathogenicity_Score']).sum() / x['Count'].sum()
-).reset_index(name='Pathogenicity_Likelihood')
-
-final_data_temp = sig_data.melt(id_vars=['sample'], var_name='Signature', value_name='Contribution')
-
-# Merge the melted data with likelihoods
-temp_merged_data = pd.merge(final_data_temp, weighted_likelihood, on='Signature', how='left')
-merged_data = temp_merged_data.dropna(subset=['Contribution', 'Pathogenicity_Likelihood'])
-
-merged_data['Weighted_Pathogenicity'] = merged_data['Contribution'] * merged_data['Pathogenicity_Likelihood']
-
-# Add the weighted likelihood for each sample
-sample_pathogenicity = merged_data.groupby('sample')['Weighted_Pathogenicity'].sum().reset_index()
-
-def map_pathogenicity(likelihood):
-    if likelihood >= pathogenicity_mapping['Pathogenic']:
-        return 'Pathogenic'
-    elif likelihood >= pathogenicity_mapping['Likely Pathogenic']:
-        return 'Likely Pathogenic'
-    elif likelihood >= pathogenicity_mapping['Possibly pathogenic']:
-        return 'Possibly pathogenic'
-    elif likelihood >= pathogenicity_mapping['VUS']:
-        return 'VUS'
-    else:
-        return 'Benign'
-
-sample_pathogenicity['Pathogenicity_Category'] = sample_pathogenicity['Weighted_Pathogenicity'].apply(map_pathogenicity)
-
-print(sample_pathogenicity)
+print(result)
 print("\n--------------------------\n")
 
-X = sample_pathogenicity.drop(columns=['sample', 'Pathogenicity_Category'])
-y = sample_pathogenicity['Pathogenicity_Category']
+#############################
+####### TP53 Database #######
+#############################
+new_file_tester =  '../../../Downloads/UMD_variants_EU.xlsx'
+tester_data =  pd.read_excel(new_file_tester)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-clf = RandomForestClassifier(random_state=42)
-clf.fit(X_train, y_train)
+print(tester_data)
 
-y_pred = clf.predict(X_test)
 
-print("Accuracy:", accuracy_score(y_test, y_pred))
-print("Classification Report:\n", classification_report(y_test, y_pred))
+
+
+
+
+# temp = tester_data[tester_data['Mutational_event'].str.contains('^[A, C, G, T]>[A, C, G, T]$') & 
+#                     tester_data['WT_Codon'].str.contains('^[ACGT]{3}$') & 
+#                     tester_data['Mutant_Codon'].str.contains('^[ACGT]{3}$')]
+
+# temp['Signature'] = temp.apply(lambda row: row['Mutant_Codon'][0] + '[' + row['Mutational_event'] + ']' + row['Mutant_Codon'][2], axis=1)
+# filtered = temp[['Variant_Classification', 'Pathogenicity', 'Final comment',  'Signature']]
+
+# grouped_signature_data = filtered.groupby(['Signature', 'Pathogenicity']).size().reset_index(name='Count')
+
+# pathogenicity_mapping = {
+#     'Pathogenic': 1,
+#     'Likely Pathogenic': 0.75,
+#     'Possibly pathogenic': 0.5,
+#     'VUS': 0.25,
+#     'Benign': 0
+# }
+
+# grouped_signature_data['Pathogenicity_Score'] = grouped_signature_data['Pathogenicity'].map(pathogenicity_mapping)
+
+# weighted_likelihood = grouped_signature_data.groupby('Signature').apply(
+#     lambda x: (x['Count'] * x['Pathogenicity_Score']).sum() / x['Count'].sum()
+# ).reset_index(name='Pathogenicity_Likelihood')
+
+# final_data_temp = sig_data.melt(id_vars=['sample'], var_name='Signature', value_name='Contribution')
+
+# # Merge the melted data with likelihoods
+# temp_merged_data = pd.merge(final_data_temp, weighted_likelihood, on='Signature', how='left')
+# merged_data = temp_merged_data.dropna(subset=['Contribution', 'Pathogenicity_Likelihood'])
+
+# merged_data['Weighted_Pathogenicity'] = merged_data['Contribution'] * merged_data['Pathogenicity_Likelihood']
+
+# # Add the weighted likelihood for each sample
+# sample_pathogenicity = merged_data.groupby('sample')['Weighted_Pathogenicity'].sum().reset_index()
+
+# def map_pathogenicity(likelihood):
+#     if likelihood >= pathogenicity_mapping['Pathogenic']:
+#         return 'Pathogenic'
+#     elif likelihood >= pathogenicity_mapping['Likely Pathogenic']:
+#         return 'Likely Pathogenic'
+#     elif likelihood >= pathogenicity_mapping['Possibly pathogenic']:
+#         return 'Possibly pathogenic'
+#     elif likelihood >= pathogenicity_mapping['VUS']:
+#         return 'VUS'
+#     else:
+#         return 'Benign'
+
+# sample_pathogenicity['Pathogenicity_Category'] = sample_pathogenicity['Weighted_Pathogenicity'].apply(map_pathogenicity)
+
+# print('\n', sample_pathogenicity)
+# print("\n--------------------------\n")
+
+# X = sample_pathogenicity.drop(columns=['sample', 'Pathogenicity_Category'])
+# y = sample_pathogenicity['Pathogenicity_Category']
+
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# clf = RandomForestClassifier(random_state=42)
+# clf.fit(X_train, y_train)
+
+# y_pred = clf.predict(X_test)
+
+# print("Accuracy:", accuracy_score(y_test, y_pred))
+# print("Classification Report:\n", classification_report(y_test, y_pred))
